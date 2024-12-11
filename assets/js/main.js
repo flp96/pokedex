0776adbef0b35d9d423caaf52f1c5bb5fbb185c1
flp96 <filipefernandes__@live.com>
2024-12-11 03:04:41 -0300

    paginacao limitada na primeira geracao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-const limit = 5;
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
  function loadPokemonItens(offset, limit) {
@@ -28,7 +29,17 @@ loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
   offset += limit;
-  loadPokemonItens(limit, offset);
+
+  const qtdRecordNextPage = offset + limit  
+
+  if(qtdRecordNextPage >= maxRecords) {
+    const newLimit = maxRecords - offset
+    loadPokemonItens(offset, newLimit)
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+  } else {
+    loadPokemonItens(offset, limit)
+  }
 })
 
   
